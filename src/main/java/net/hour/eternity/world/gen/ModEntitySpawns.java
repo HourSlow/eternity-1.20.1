7246fb2cb922af04ab27bb988d5f742ec7c95ea2
Hour <andrija.pusic2009@gmail.com>
2025-08-07 18:20:35 +0200

    Eternity, Commit 20: Updated gretchet and forgotten spawns

--- src/main/java/net/hour/eternity/world/gen/ModEntitySpawns.java
+++ src/main/java/net/hour/eternity/world/gen/ModEntitySpawns.java
@@ -7,14 +7,21 @@ import net.hour.eternity.world.biome.ModBiome;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.world.Heightmap;
 
 public class ModEntitySpawns {
     public static void addSpawns() {
         BiomeModifications.addSpawn(BiomeSelectors.includeByKey(ModBiome.WASTES),
-                SpawnGroup.MONSTER, ModEntities.THE_FORGOTTEN, 100,1,2);
+                SpawnGroup.MONSTER, ModEntities.THE_FORGOTTEN, 20,1,1);
 
         SpawnRestriction.register(ModEntities.THE_FORGOTTEN, SpawnRestriction.Location.ON_GROUND,
                 Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, HostileEntity::canSpawnInDark);
+
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(ModBiome.EERIE_FOREST),
+                SpawnGroup.CREATURE, ModEntities.LILGUY, 80,2,4);
+
+        SpawnRestriction.register(ModEntities.LILGUY, SpawnRestriction.Location.ON_GROUND,
+                Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::canMobSpawn);
     }
 }
